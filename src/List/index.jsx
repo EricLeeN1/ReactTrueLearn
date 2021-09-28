9f9bc34e0c3a02ca56308ea6bc2b1ee6912716a3
EricLeeN1 <179310009@qq.com>
2021-09-28 16:25:42 +0800

    feac:github用户卡片头像

---
+++ src/List/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Card from "../Card";
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="row">
+        <Card></Card>
+      </div>
+    );
+  }
+}
