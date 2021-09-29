02fc65c1b3e1a07d38843c515443aa4ef9a59291
EricLeeN1 <179310009@qq.com>
2021-09-29 10:01:19 +0800

    feac:fetch demo

---
+++ fetch demo/List/index.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Card from "../Card";
+import PubSub from "pubsub-js";
+import "./index.css";
+
+export default class List extends Component {
+  state = {
+    users: [], // users初始值为数组
+    isFirst: true, // 是否为第一次打开页面
+    isLoading: false, // 标识是否处于加载中
+    err: "", // 存储请求相关的错误信息
+  };
+
+  componentDidMount() {
+    PubSub.subscribe("updateState", (msg, data) => {
+      console.log(msg, data);
+      this.setState(data)
+    });
+  }
+
+  render() {
+    const { users, isFirst, isLoading, err } = this.state;
+    return (
+      <div className="row">
+        { isFirst ? (
+          <h2>输入关键字随后点击搜索</h2>
+        ) : isLoading ? (
+          <h2>loading...</h2>
+        ) : err ? (
+          <h2>{err.message}</h2>
+        ) : (
+          users.map((item) => {
+            return <Card key={item.id} item={item}></Card>;
+          })
+        )}
+      </div>
+    );
+  }
+}
---
+++ fetch demo/Search/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from "axios";
+import PubSub from "pubsub-js";
+export default class Search extends Component {
+  keywordRef = React.createRef();
+
+  search = async () => {
+    //获取用户输入，(连续解构赋值+重命名)
+    const {
+      keywordRef: {
+        current: { value: data },
+      },
+    } = this;
+    PubSub.publish("updateState", {
+      isFirst: false,
+      isLoading: true,
+    });
+    // 发送网络请求
+
+    try {
+      const response = await fetch(`/api/search/users?q=${data}`);
+      const res = await response.json();
+      PubSub.publish("updateState", {
+        users: res.items,
+        isLoading: false,
+      });
+    } catch (error) {
+      PubSub.publish("updateState", {
+        err: error,
+        isLoading: false,
+      });
+    }
+  };
+  render() {
+    return (
+      <section className="jumbotron">
+        <h3 className="jumbotron-heading">Search Github Users</h3>
+        <div>
+          <input
+            ref={this.keywordRef}
+            type="text"
+            placeholder="enter the name you search"
+          />
+          &nbsp;<button onClick={this.search}>Search</button>
+        </div>
+      </section>
+    );
+  }
+}
