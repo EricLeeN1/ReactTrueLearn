6eb4909697227a13a5c526bc4d1c0949669b0f9a
EricLeeN1 <179310009@qq.com>
2021-10-11 17:05:50 +0800

    feac:react-redux情况下不用在index中监测reactDOM.render

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import store from "./redux/store";
 ReactDOM.render(
   // 检查react语法
   <React.StrictMode>
@@ -14,19 +13,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// 检测redux中状态的改变，如redux的状态发生了改变，那么重新渲染App组件
-store.subscribe(() => {
-  ReactDOM.render(
-    // 检查react语法
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
