c012199d856070b2127c9c2ab2a0347a5b283196
EricLeeN1 <179310009@qq.com>
2021-10-12 11:10:47 +0800

    feac:effectHooks

--- extend.md
+++ extend.md
@@ -65,3 +65,25 @@
 (4). setXxx()2种写法:
         setXxx(newValue): 参数为非函数值, 直接指定新的状态值, 内部用其覆盖原来的状态值
         setXxx(value => newValue): 参数为函数, 接收原本的状态值, 返回新的状态值, 内部用其覆盖原来的状态值
+
+#### 4. Effect Hook
+
+```
+(1). Effect Hook 可以让你在函数组件中执行副作用操作(用于模拟类组件中的生命周期钩子)
+(2). React中的副作用操作:
+        发ajax请求数据获取
+        设置订阅 / 启动定时器
+        手动更改真实DOM
+(3). 语法和说明: 
+        useEffect(() => { 
+          // 在此可以执行任何带副作用操作
+          return () => { // 在组件卸载前执行
+            // 在此做一些收尾工作, 比如清除定时器/取消订阅等
+          }
+        }, [stateValue]) // 如果指定的是[], 回调函数只会在第一次render()后执行
+    
+(4). 可以把 useEffect Hook 看做如下三个函数的组合
+        componentDidMount()
+        componentDidUpdate()
+    	componentWillUnmount() 
+```
--- src/components/3_hooks/index.jsx
+++ src/components/3_hooks/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDom from "react-dom";
 
 // export default class index extends Component {
 //   state = {
@@ -10,12 +11,25 @@ import React, { Component } from "react";
 //       count: count + 1,
 //     }));
 //   };
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       console.log(this.state.count);
+//       this.setState((state) => ({ count: state.count + 1 }));
+//     }, 1000);
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+//   unmount = () => {
+//     ReactDom.unmountComponentAtNode(document.getElementById("root"));
+//   };
 //   render() {
 //     const { count } = this.state;
 //     return (
 //       <div>
 //         <h1>当前求和为{count}</h1>
 //         <button onClick={this.add}>点我+1</button>
+//         <button onClick={this.unmount}>卸载组件</button>
 //       </div>
 //     );
 //   }
@@ -23,16 +37,31 @@ import React, { Component } from "react";
 
 function Demo() {
   // 调用1+n次 初次渲染一次，之后每次状态更新一次
-  console.log("Demo");
+  // console.log("Demo");
   const [count, setCount] = React.useState(0);
-  const [name, setName] = React.useState('eric');
+  const [name, setName] = React.useState("eric");
+
+  React.useEffect(() => {
+    // 相当于生命周期钩子
+    console.log("@");
+    const timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []); // 监测count
   // 加的回调
   function add() {
     setCount((count) => count + 1);
   }
   // 改名的回调
   function changeName() {
-    setName((name) => name + 'Lee');
+    setName((name) => name + "Lee");
+  }
+
+  function unmount() {
+    ReactDom.unmountComponentAtNode(document.getElementById("root"));
   }
   return (
     <div>
@@ -40,6 +69,7 @@ function Demo() {
       <h1>我的名字为{name}</h1>
       <button onClick={add}>点我+1</button>
       <button onClick={changeName}>改名字了</button>
+      <button onClick={unmount}>卸载组件</button>
     </div>
   );
 }
